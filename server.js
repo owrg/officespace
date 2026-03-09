f0938971fbb11e30235c54081b508652099ade5c
Oliver Guildford <oliver.guildford@cgi.com>
2026-03-09 10:47:26 +0000

    Updated room logic

--- server.js
+++ server.js
@@ -8,27 +8,40 @@ app.use(express.static('public')); // This serves your HTML/JS files
 let players = {};
 
 io.on('connection', (socket) => {
-    const room = socket.handshake.query.room;
-    socket.join(room); // This puts the user in a private "room"
-    console.log('A user connected:', socket.id);
+    const room = socket.handshake.query.room || 'lobby';
+    console.log(`User ${socket.id} joined room: ${room}`);
+    socket.join(room);;
 
-    // Create a new player object
-    players[socket.id] = { x: 400, y: 300, color: '#' + Math.floor(Math.random()*16777215).toString(16) };
+// Create player state
+    players[socket.id] = { 
+        x: 400, 
+        y: 300, 
+        color: '#' + Math.floor(Math.random()*16777215).toString(16),
+        room: room 
+    };
 
-    // Tell the new player about everyone else, and tell everyone about the new player
-    socket.emit('currentPlayers', players);
+// ONLY send existing players who are in the SAME room
+    const playersInRoom = {};
+    Object.keys(players).forEach((id) => {
+        if (players[id].room === room) {
+            playersInRoom[id] = players[id];
+        }
+    });
+    socket.emit('currentPlayers', playersInRoom);
+
+    // Tell others in THAT ROOM a new player joined
     socket.to(room).emit('newPlayer', { id: socket.id, player: players[socket.id] });
 
-    // When a player moves, update the 'Brain' and tell others
+    // When moving, only broadcast to people in the same room
     socket.on('playerMovement', (movementData) => {
         players[socket.id].x = movementData.x;
         players[socket.id].y = movementData.y;
-        socket.broadcast.emit('playerMoved', { id: socket.id, x: players[socket.id].x, y: players[socket.id].y });
+        socket.to(room).emit('playerMoved', { id: socket.id, x: players[socket.id].x, y: players[socket.id].y });
     });
 
     socket.on('disconnect', () => {
         delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
+        io.to(room).emit('playerDisconnected', socket.id);
     });
 });
 
